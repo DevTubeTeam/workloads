b33f49e3a3c1ccc54d279b2bd79f7078e1a6bf61
quocanh944 <quocanh944@gmail.com>
2025-07-02 22:55:44 +0700

    feat: modify k8s.yaml and add new folder tf

---
+++ tf/0-locals.tf
@@ -0,0 +1,8 @@
+locals {
+  env         = "prod"
+  region      = "ap-southeast-1"
+  zone1       = "ap-southeast-1a"
+  zone2       = "ap-southeast-1b"
+  eks_name    = "devtube"
+  eks_version = "1.32"
+}
